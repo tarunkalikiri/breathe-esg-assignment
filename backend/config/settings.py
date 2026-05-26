46b9e5736611bfed1b00b460bcca9eb801276e8f
Tarun <kalikiritarun@gmail.com>
2026-05-27 02:09:50 +0530

    Fixed allowed hosts

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rvax4z$o5o4zvh(mwuvz&m221xbiakxi8xq4*n38b#34!!t0!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'breathe-esg-assignment-sf55.onrender.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
